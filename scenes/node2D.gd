083192f564886b3d06bd17b9f9013641f6cde0db
tekkenfede <federico.mele1998@gmail.com>
2023-08-26 17:30:51 +0200

    Update Handheld version to current github version. Ui rescaled for touch and little devices with different form factor (640x480)

---
+++ scenes/node2D.gd
@@ -0,0 +1,10 @@
+
+
+extends Node2D
+
+
+
+func _process(delta):
+
+	var mousePosition = get_global_mouse_position()
+	position = mousePosition
--- scenes/panelUnlocks.gd
+++ scenes/panelUnlocks.gd
@@ -8,13 +8,13 @@ func _ready() -> void:
 	global.nUnlockPanel=self
 
 func showMessage(message) -> void:
-	var targetY = 228#240 if global.nCtnStructures.bHidden else 210
+	var targetY = 350#240 if global.nCtnStructures.bHidden else 210
 	$label.text = message
 	var _v = $tween.interpolate_property(self, 'rect_global_position:y', self.rect_global_position.y, targetY, 0.3, Tween.TRANS_QUINT, Tween.EASE_OUT)
 	_v = $tween.start()
 	
 	yield(get_tree().create_timer(3.0), "timeout")
-	_v = $tween.interpolate_property(self, 'rect_global_position:y', self.rect_global_position.y, 360, 0.3, Tween.TRANS_QUINT, Tween.EASE_IN)
+	_v = $tween.interpolate_property(self, 'rect_global_position:y', self.rect_global_position.y, 550, 0.3, Tween.TRANS_QUINT, Tween.EASE_IN)
 	_v = $tween.start()
 
 func oneKill() -> void:
